d9a8573282257bf0ad35a48f49b5639794255233
Nathaniel Ledford <nate@nateledford.com>
2024-06-16 17:26:34 -0400

    Fix issues with custom logger config

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,24 @@
 use anyhow::Result;
+use chrono::Local;
 use clap::Parser;
-use log::LevelFilter;
-use simplelog::{ColorChoice, CombinedLogger, ConfigBuilder, TerminalMode, TermLogger};
+use log::*;
+use simplelog::*;
+use time::UtcOffset;
 
 use chidori::cli;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    CombinedLogger::init(vec![TermLogger::new(
+    let logger_config = ConfigBuilder::new()
+        .set_time_offset(UtcOffset::from_whole_seconds(Local::now().offset().local_minus_utc()).unwrap())
+        .build();
+
+    TermLogger::init(
         LevelFilter::Trace,
-        ConfigBuilder::default()
-            .set_time_offset_to_local()
-            .unwrap()
-            .build(),
+        logger_config,
         TerminalMode::Mixed,
         ColorChoice::Auto,
-    )])?;
+    )?;
 
     // config::delete_config_file().await;
 
