066efee5abd60d2fd818d619456c6c265fbd77a3
Nathaniel Ledford <nate@nateledford.com>
2024-06-18 14:48:51 -0400

    Add documentation

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,8 @@
+//! Represents the application state
+//!
+//! Loads the application configuration file and profiles from disk, as well as building a
+//! [`PlexClient`](crate::plex::PlexClient) and loading playlists from the Plex server.
+
 use anyhow::Result;
 use derive_builder::Builder;
 
@@ -9,13 +14,21 @@ use crate::profiles::profile::Profile;
 /// Represents the application state
 #[derive(Builder, Clone, Debug, Default)]
 pub struct AppState {
+    /// The application's configuration file
     config: Config,
+    /// A wrapper for the Plex API
     plex_client: PlexClient,
+    /// [`playlist`](crate::plex::models::Playlist)s fetched from Plex
     playlists: Vec<Playlist>,
+    /// [`profile`](crate::profiles::profile::Profile)s loaded from disk
     profiles: Vec<Profile>,
 }
 
 impl AppState {
+    /// Initializes the application state by loading a configuration file from disk (or creating one
+    /// if it does not exist) and loading existing profiles, if any, from the disk.
+    /// A ['PlexClient'](crate::plex::PlexClient) is then created, which is used to load playlists
+    /// from the Plex server.
     pub async fn initialize() -> Result<Self> {
         let config = Config::load_config().await?;
 
@@ -36,6 +49,8 @@ impl AppState {
 
 // Playlists
 impl AppState {
+    /// Searches for a [`Playlist`](crate::plex::models::Playlist) by its title from the
+    /// application state
     pub fn get_playlist_by_title(&self, title: &str) -> Option<&Playlist> {
         self.playlists.iter().find(|p| p.title == title)
     }
@@ -43,6 +58,7 @@ impl AppState {
 
 // Plex
 impl AppState {
+    /// Returns a reference to the [`PlexClient`](crate::plex::PlexClient) from the application state
     pub fn get_plex_client(&self) -> &PlexClient {
         &self.plex_client
     }
@@ -50,6 +66,7 @@ impl AppState {
 
 // Profiles
 impl AppState {
+    /// Returns a `vec` of enabled profiles loaded in the application state
     pub fn get_enabled_profiles(&self) -> Vec<Profile> {
         self.profiles
             .iter()
@@ -63,6 +80,7 @@ impl AppState {
             .collect::<Vec<Profile>>()
     }
 
+    /// Returns a `vec` of titles from all profiles loaded in the application state
     pub fn get_profile_titles(&self) -> Vec<&str> {
         self.profiles
             .iter()
@@ -70,18 +88,23 @@ impl AppState {
             .collect::<Vec<&str>>()
     }
 
-    pub fn get_profile(&self, title: &str) -> Option<&Profile> {
+    /// Searches for a specific [`profile`](crate::profiles::profile::Profile) by its title.
+    /// Returns `None` if no profile can be found.
+    pub fn get_profile_by_title(&self, title: &str) -> Option<&Profile> {
         self.profiles.iter().find(|p| p.get_title() == title)
     }
 
+    /// Returns the directory where ['profile'](crate::profiles::profile::Profile)s are stored on disk.
     pub fn get_profiles_directory(&self) -> &str {
         self.config.get_profiles_directory()
     }
 
+    /// Checks if [`profile`](crate::profiles::profile::Profile)s have been loaded to the application state.
     pub fn have_profiles(&self) -> bool {
         !self.profiles.is_empty()
     }
 
+    /// Prints a list of all [`profile`](crate::profiles::profile::Profile)s loaded from disk
     pub fn list_profiles(&self) {
         let mut titles = self
             .profiles
