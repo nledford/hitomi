4ae393484d37dd43f99853b22dde40e7d7e75a37
Nathaniel Ledford <nate@nateledford.com>
2024-06-21 15:53:39 -0400

    Remove unused imports

--- src/profiles/types.rs
+++ src/profiles/types.rs
@@ -1,13 +1,6 @@
-// Derives inside the nutype macro are not recognized by the compiler so to suppress the warnings,
-// allow "unused" imports
-#![allow(unused_imports)]
-
-use std::fmt::Display;
-
 use nutype::nutype;
 use once_cell::sync::Lazy;
 use regex::Regex;
-use serde::{Deserialize, Serialize};
 
 static PROFILE_SOURCE_ID_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^\d+$").unwrap());
 
